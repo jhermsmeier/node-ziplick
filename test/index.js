37649a9e32d06189bbf5f4f05cf5e746975e4dd8
Jonas Hermsmeier <jhermsmeier@gmail.com>
2015-09-07 02:03:33 +0200

    Stub tests

---
+++ test/index.js
@@ -0,0 +1,8 @@
+describe( 'Zip', function() {
+  
+  it( 'errors on empty zip file', function() {
+    // 4.1.2 ZIP files SHOULD contain at least one file and MAY contain 
+    // multiple files.
+  })
+  
+})
